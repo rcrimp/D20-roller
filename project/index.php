44d8d004d017e5b1c5fa2a4edb0080da98b916f1
Reuben Crimp <reubencrimp@gmail.com>
2017-09-19 23:35:14 +1200

    testing cache control

--- project/index.php
+++ project/index.php
@@ -1,7 +1,18 @@
-<?php 
-$timestamp = time();
-//header or readfile ??
-//echo('Location: roll.php?fbrefresh=' . $timestamp);
-header('Location: roll.php?fbrefresh='.$timestamp);
+<?php
+header("Vary: User-Agent, Accept");
 ?>
 
+<?php 
+$diceroll = rand(1,20); 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    	<title>You rolled a <?php echo($diceroll) ?></title>
+	</head>
+	<body>
+		<img src = 'img/<?php echo($diceroll) ?>.png'>
+		<a href="github.com/rcrimp/D20-roller">Github</a>
+	</body>
+</html>
